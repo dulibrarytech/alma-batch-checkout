da9664e832d06e7ebc22c69939633654e7eece11
Jeff <you@example.com>
2017-11-12 20:09:56 -0700

    Refactor db for separate loan collection; add section label styles

--- schema.js
+++ schema.js
@@ -7,14 +7,27 @@ db.createCollection( "abcdb_set",
             { "data.createDate" : { $type: "date" } },
             { "data.period" : { $type: "number" } },
             { "status" : { $in: ["AVAILABLE", "ON_LOAN"] } },
-            { "patronID" : { $type: "string" } },
-            { "due" : { $type: "date" } },
+            { "loanID" : { $type: "string" } },
             { "items" : { $type: "string" } }
          ]
       },
       validationAction: "error"
    }
 )
+
+db.createCollection( "abcdb_loan",
+   {
+      validator: { $and:
+         [
+            { "setID" : { $type: "string" } },
+            { "userID" : { $type: "string" } },
+            { "due" : { $type: "date" } }
+         ]
+      },
+      validationAction: "error"
+   }
+)
+
 
 
 
