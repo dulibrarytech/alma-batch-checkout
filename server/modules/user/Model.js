f3800eb2f8cffe8cf57c934927144d1b04ec4b8c
Jeff <you@example.com>
2018-03-25 16:14:18 -0600

    Fix function name

--- server/modules/user/Model.js
+++ server/modules/user/Model.js
@@ -30,3 +30,47 @@ exports.getUserList = function(callback) {
                 callback(e, null);
         }
 };
+
+exports.addUser = function(data, callback) {
+
+    var userData = {};
+    userData['fname'] = data.fname || "";
+    userData['lname'] = data.lname || "";
+    userData['DUID'] = data.DUID || "";
+    userData['role'] = data.role || "";
+
+    var doc = userData;
+    try {
+        collection.insertOne(doc).then(data => {
+            callback(null, data.insertedId);
+        });
+    }
+    catch(e) {
+        callback(e, null);
+    }
+}
+
+exports.updateUser = function(userID, data, callback) {
+    try {
+       collection.updateOne(
+          { _id : ObjectId(userID) },
+          { $set: { "fname" : data.fname, 
+                    "lname" : data.lname,
+                    "DUID" : data.DUID,
+                    "role" : data.role
+                  } }
+       );
+       callback(null, true);
+    } catch (e) {
+       callback(e, null);
+    }
+}
+
+exports.deleteUser = function(userID, callback) {
+    try {
+       collection.deleteOne( { "_id" : ObjectId(userID) } );
+       callback(null, true);
+    } catch (e) {
+       callback(e, null);
+    }
+}
